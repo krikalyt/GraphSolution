9ca2beb4882fbbf31c893472c2ea4fe55b48b815
krikalyt@gmail.com <krikalyt@gmail.com>
2020-05-22 11:16:53 +0530

    ArticualtionPoint

---
+++ _articulation.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+int id;
+vector<int> ids;
+vector<int> lowlink;
+void dfs(int s,int par,vector<int> edge[],bool visited[]){
+    visited[s] = true;
+    ids[s] = lowlink[s] = id;
+    id++;
+    for(int x : edge[s]){
+        if(!visited[x]){
+            dfs(x,s,edge,visited);
+            lowlink[s] = min(lowlink[s],lowlink[x]);
+        }else{
+            if(x != par){
+                lowlink[s] = min(ids[x],lowlink[s]);
+            }
+        }
+    }
+}
+set<int> ans;
+void dfs2(int s,vector<int> edge[],bool visited[]){
+    visited[s] = true;
+    for(int x : edge[s]){
+        if(!visited[x]){
+            dfs2(x,edge,visited);
+            if(ids[s] <= lowlink[x] && s!= 0){
+                ans.insert(s);
+            }
+        }
+    }
+}
+bool rootartdfs(int s,vector<int> edge[],bool visited[]){
+    visited[s] = true;
+    int i = 0;
+    for(int x : edge[s]){
+        if(!visited[x]){
+            rootartdfs(x,edge,visited);
+            i++;
+        }
+    }
+
+    if(i > 1){
+        return true;
+    }else{
+        return false;
+    }
+}
+int main()
+{
+    freopen("input.txt","r",stdin);
+    int t;
+    cin >> t;
+    while(t--){
+        int n,e;
+        cin >> n >> e;
+        vector<int> edge[n];
+        for(int i=0;i<e;i++){
+            int a,b;
+            cin >> a >> b;
+            edge[a].push_back(b);
+            edge[b].push_back(a);
+        }
+
+        bool visited[n] = {false};
+        ids.clear();
+        lowlink.clear();
+        ids.resize(n);
+        lowlink.resize(n);
+
+        dfs(0,-1,edge,visited);
+        fill(visited,visited+n,false);
+        dfs2(0,edge,visited);
+        fill(visited, visited+n,false);
+        if(rootartdfs(0,edge,visited)){
+            ans.insert(0);
+        }
+        if(ans.size() == 0){
+            cout << "No Articulation Point" << '\n';
+        }else{
+            for(int x : ans){
+                cout << x << ' ';
+            }
+            cout << '\n';
+        }
+        
+    }
+    return 0;
+}
